cfa551996764a93090dd73e115ddfd758fbfbd7e
James Randall <31254270+oljp@users.noreply.github.com>
2021-08-03 17:13:51 -0400

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -7,5 +7,24 @@ module.exports = {
     { name: 'beta', prerelease: true }
   ],
   plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/github',
+      {
+        assets: ['dist/*.tgz'],
+        
+        successComment: {
+          enabled: false
+        },
+        
+        releasedLabels: {
+          enabled: true,
+          labels: ['released on <%= nextRelease.channel ? `${nextRelease.channel}` : "test" %>']
+        }
+      }
+    ],
+    '@semantic-relase/git
   ]
 };
